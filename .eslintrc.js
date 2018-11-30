d52aaccd0a15d2347e7820f6774b4dc5956aec3a
sudame <su.glasses.923@gmail.com>
2018-11-30 22:57:10 +0900

    update: eslint's config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,33 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    "plugin:vue/essential",
+    "google",
+    "plugin:prettier/recommended"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'max-len': 0,
+    indent: ['error', 2],
+    'vue/html-indent': ['error', 2],
+    "prettier/prettier": [
+      "error",
+      {
+        semi: true,
+        singleQuote: true,
+        trailingComma: "es5"
+      }
+    ]
+  },
+};
