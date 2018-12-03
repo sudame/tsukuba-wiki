a2185ae32724237eb528e37b45d9d50ceb7be793
sudame <su.glasses.923@gmail.com>
2018-12-04 01:49:40 +0900

    update: firestore's timestamps setting

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,6 +3,7 @@ const config = require('../firebase-keys.json');
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
+  firebase.firestore().settings({ timestampsInSnapshots: true });
 }
 
 export default firebase;
