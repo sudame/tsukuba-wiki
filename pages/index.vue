ae5a4b3b1139a081b83e973f7095f5f6ca58e442
sudame <su.glasses.923@gmail.com>
2018-12-04 01:48:07 +0900

    add: get latest 5 articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <section><nuxt-link to="/login">ログイン</nuxt-link></section>
+  <section>
+    <nuxt-link to="/login">ログイン</nuxt-link>
+    <p v-for="article in articles" :key="article.id">{{ article }}</p>
+  </section>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+  created() {
+    this.$store.dispatch('setArticles');
+  },
+  computed: {
+    ...mapState(['articles']),
+  },
+};
 </script>
 
 <style></style>
--- store/index.js
+++ store/index.js
@@ -1,16 +1,32 @@
 import Vuex from 'vuex';
+import firebase from '../plugins/firebase';
+import { firebaseAction, firebaseMutations } from 'vuexfire';
 
 const createStore = () => {
   return new Vuex.Store({
     state: () => ({
       // ログイン状態(firebase.User || false)
       auth: false,
+      articles: [],
     }),
     mutations: {
       // ログイン状態を変更する
       authStateChanger(state, user) {
         state.auth = user;
       },
+      ...firebaseMutations,
+    },
+    actions: {
+      setArticles: firebaseAction(({ bindFirebaseRef }) => {
+        bindFirebaseRef(
+          'articles',
+          firebase
+            .firestore()
+            .collection('articles')
+            .orderBy('created_at', 'desc')
+            .limit(5)
+        );
+      }),
     },
   });
 };
